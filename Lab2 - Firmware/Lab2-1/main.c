9c5ff2f7ea09eac5759f3c84209904657f394e8f
Kyle Ah Von <73436800+Koi199@users.noreply.github.com>
2025-09-25 14:48:34 -0700

    Lab2 Exercise 1 and Exercise 2
    Intro to firmware

---
+++ Lab2 - Firmware/Lab2-1/main.c
@@ -0,0 +1,28 @@
+#include "msp430.h"
+
+// #define WDTPW     (0x5A00)  /* Watchdog Timer Password */
+// #define WDTHOLD   (0x0080)  /* Watchdog Timer Hold */
+
+void main (void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+
+    CSCTL0 = CSKEY; // Set default password to clock 
+
+    CSCTL1 |= DCOFSEL_3; // Select lower frequency range from DCORSEL
+
+    CSCTL2 |= SELS__DCOCLK; // Set SMCLK to DCO
+
+    CSCTL3 |= DIVS__32; // Set the Div to 32
+
+    // Set up P3.4 to output tertiary function SMCLK
+    P3DIR |= BIT4;
+    P3SEL0 |= BIT4;
+    P3SEL1 |= BIT4;
+
+    // Super loop (optional here, but good practice)
+    while (1) {
+        _NOP();       // CPU idle, clock keeps running on P3.4
+    }
+
+}
---
+++ Lab2 - Firmware/Lab2-2/main.c
@@ -0,0 +1,39 @@
+#include "msp430.h"
+//******************************************************************************
+//!
+//!   Empty Project that includes driverlib
+//!
+//******************************************************************************
+
+void main (void)
+{   
+    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+    // Set up the clock 
+    CSCTL0 = CSKEY; // Set default password to clock 
+    CSCTL1 |= DCOFSEL_3; // Select lower frequency range from DCORSEL
+    CSCTL2 |= SELS__DCOCLK; // Set SMCLK to DCO
+    CSCTL3 |= DIVS__1; // Set the Div to 1
+    
+    // Configure pins to output
+    PJDIR |= BIT0 | BIT1 | BIT2 | BIT3;
+    PJSEL0 &=  ~(BIT0 | BIT1 | BIT2 | BIT3);
+    PJSEL1 &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+
+    P3DIR |= BIT4 | BIT5 | BIT6 | BIT7;
+    P3SEL0 &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+    P3SEL1 &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+
+    PJOUT |= (BIT0 | BIT3);
+    P3OUT |= (BIT6 | BIT7);
+
+    while(1)
+    { 
+
+        PJOUT ^= (BIT1|BIT2);
+        P3OUT ^= (BIT4|BIT5);
+
+        _delay_cycles(1000000);
+
+    }
+}
